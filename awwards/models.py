7eab659e05595433e3dac2581a4710826f3a69d7
ivymmurithi <ivymurithi@gmail.com>
2022-03-12 14:29:26 +0300

    make db models

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='profiles/',null=False, blank=False)
+    bio = models.TextField(null=False, blank=False)
+    contact_info = models.CharField(max_length=30 ,null=False, blank=False)
+
+class Project(models.Model):
+    title = models.CharField(max_length=30,null=False, blank=False,)
+    website_picture = models.ImageField(upload_to='uploads/',null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    website_link = models.CharField(max_length=255,null=False, blank=False)
