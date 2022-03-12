487c59a4c67ac6d9074c62d1efdc3d48028510d1
ivymmurithi <ivymurithi@gmail.com>
2022-03-12 16:47:49 +0300

    db migrations + uppload profile picture

--- awwards/models.py
+++ awwards/models.py
@@ -1,9 +1,10 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    profile_picture = models.ImageField(upload_to='profiles/',null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/',null=True, blank=True, default='profiles/222.jpg')
     bio = models.TextField(null=True, blank=True)
     contact_info = models.CharField(max_length=30 ,null=True, blank=True)
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
