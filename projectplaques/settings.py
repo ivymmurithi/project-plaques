b061f2ba3816dd48dcd3cd05c520563a4867d774
ivymmurithi <ivymurithi@gmail.com>
2022-03-12 13:46:14 +0300

    setup db

--- projectplaques/settings.py
+++ projectplaques/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!1#^@m=!3p-yu#o53$=ap7ja@e(ta6$lt*ri5g+0=_-)@rzf!7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'projectplaques.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('NAME'),
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD')
     }
 }
 
