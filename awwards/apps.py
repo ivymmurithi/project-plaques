48eae4b9f10661b7a007b6c28f9bf52eb917a154
ivymmurithi <ivymurithi@gmail.com>
2022-03-12 16:44:46 +0300

    add post_save signal to save profile after creation

--- awwards/apps.py
+++ awwards/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AwwardsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'awwards'
+
+    """
+    connect the signals file with the app.py file ready function in order to use them.
+    """
+    def ready(self):
+        import awwards.signals
